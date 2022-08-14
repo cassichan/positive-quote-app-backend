ad54c0a1f46184a288492d2dafc441520628e6c3
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-14 15:39:20 -0400

    adding firebase functions

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import functions from "firebase-functions";
+import { getAllQuotes, addQuote } from "../src/functions.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/all-quotes", getAllQuotes);
+app.post("/add-quote", addQuote);
+
+export const api = functions.https.onRequest(app);
