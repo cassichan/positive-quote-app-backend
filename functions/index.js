4c256b7bbe2bfebe0ea22ebfe8a15d76648adfeb
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-15 20:10:34 -0400

    fixed api connection issue

--- functions/index.js
+++ functions/index.js
@@ -6,9 +6,18 @@ import { getAllQuotes, addQuote } from "./functions.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://inspirational-quotes-cc.web.app",
+      // "https://inspirational-quotes-cc.web.app/all-quotes",
+      "http://localhost:3000",
+    ],
+  })
+);
 
 app.get("/all-quotes", getAllQuotes);
 app.post("/add-quote", addQuote);
 
 export const api = functions.https.onRequest(app);
+// exports.app = functions.https.onRequest(app)
